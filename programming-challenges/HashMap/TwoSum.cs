6e393b1c836e3579a1737aae48b28f786323c3c8
João Augusto <joaoaugustojob99@gmail.com>
2025-07-26 12:35:00 -0500

    feat: add explanation of how to solve TwoSum Challenge

--- programming-challenges/HashMap/TwoSum.cs
+++ programming-challenges/HashMap/TwoSum.cs
@@ -3,19 +3,28 @@
 public class TwoSum
 {
     // https://leetcode.com/problems/two-sum/description/
-    public int[] TwoSumSolution(int[] nums, int target) {
-        var dic1 = new Dictionary<int, int>();
-
-        for (var i = 0; i < nums.Length; i++) {
-            var difference = target - nums[i];
-
-            if (dic1.ContainsKey(difference)) {
-                return new int[] { dic1[difference], i };
+    public static int[] TwoSumSolution(int[] nums, int target) {
+        // Create a dictionary to store the numbers and their indices
+        Dictionary<int, int> numDict = new Dictionary<int, int>();
+        
+        // Iterate through the array
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+            
+            // Check if the complement exists in the dictionary
+            // If it does, return the indices of the two numbers
+            if (numDict.ContainsKey(complement))
+            {
+                return new[] { numDict[complement], i };
             }
-
-            dic1[nums[i]] = i;
+            
+            // Add the current number and its index to the dictionary
+            // Use TryAdd to avoid exceptions if the key already exists
+            numDict.TryAdd(nums[i], i);
         }
-
-        return new int[] {0};
+        
+        // If no solution is found, return an empty array
+        return new int[0];
     }
 }
